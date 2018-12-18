82144a165e8ad5f173ffc2d4fc3f44be4d4184e2
Liad222 <liadlivni@gmail.com>
2018-12-18 09:19:32 +0200

    18.12.2018

---
+++ ex3/cmake-build-debug/CMakeFiles/ex3.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex3.dir/Code_Skeleton/Part2/Game.cpp.obj"
+  "CMakeFiles/ex3.dir/Code_Skeleton/Part2/main.cpp.obj"
+  "CMakeFiles/ex3.dir/Code_Skeleton/Part2/utils.cpp.obj"
+  "ex3.pdb"
+  "ex3.exe"
+  "ex3.exe.manifest"
+  "libex3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
