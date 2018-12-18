449b44d395661ea14ae666d3845469864d20b854
Liad222 <liadlivni@gmail.com>
2018-12-18 10:23:13 +0200

    18.12.2018

--- ex3/Code_Skeleton/Part1/PCQueue.hpp
+++ ex3/Code_Skeleton/Part1/PCQueue.hpp
@@ -4,12 +4,64 @@
 // Single Producer - Multiple Consumer queue
 template <typename T>class PCQueue
 {
-
 public:
+	PCQueue(){
+		producerInside = 0;
+		consumerInside = 0;
+		producerWaiting = 0;
+		pthread_cond_init(producerCond,NULL);
+		pthread_cond_init(consumerCond,NULL);
+		pthread_mutexattr_t attr;            //lock init
+		pthread_mutexattr_init(&attr);
+		pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
+		pthread_mutex_t error_check_mutex;
+		pthread_mutex_init(&error_check_mutex, &attr);
+		GLock = error_check_mutex;
+		queue<T> new_items;
+		items = new_items;
+	}
+
+	void consumerLock(){
+		pthread_mutex_lock(&GLock);
+		while(producerInside > 0 || producerWaiting > 0)
+				pthread_cond_wait(consumerCond,&GLock);
+		consumerInside++;
+		pthread_mutex_unlock(&GLock);
+	}
+	void consumerUnlock(){
+		pthread_mutex_lock(&GLock);
+		consumerInside--;
+		if(consumerInside == 0)
+			pthread_cond_signal(producerCond);
+		pthread_mutex_unlock(&GLock);
+	}
 	// Blocks while queue is empty. When queue holds items, allows for a single
 	// thread to enter and remove an item from the front of the queue and return it.
 	// Assumes multiple consumers.
-	T pop();
+	T pop(){
+		consumerLock();
+		if
+	}
+
+	void producerLock(){
+		pthread_mutex_lock(&GLock);
+		producerWaiting++;
+		while(producerInside + consumerInside > 0)
+			pthread_cond_wait(producerCond,&GLock);
+		producerWaiting--;
+		producerInside++;
+		pthread_mutex_unlock(&GLock);
+	}
+
+	void producerUnlock(){
+		pthread_mutex_lock(&GLock);
+		producerInside--;
+		if(producerInside == 0) {
+			pthread_cond_broadcast(consumerCond);
+			pthread_cond_signal(producerCond);
+		}
+		pthread_mutex_unlock(&GLock);
+	}
 
 	// Allows for producer to enter with *minimal delay* and push items to back of the queue.
 	// Hint for *minimal delay* - Allow the consumers to delay the producer as little as possible.
@@ -18,7 +70,11 @@ public:
 
 
 private:
-	// Add your class memebers here
+	int producerInside,consumerInside,producerWaiting;
+	pthread_mutex_t GLock;
+	pthread_cond_t *producerCond;
+	pthread_cond_t *consumerCond;
+	queue<T> items;
 };
 // Recommendation: Use the implementation of the std::queue for this exercise
 #endif
--- ex3/Code_Skeleton/Part1/Semaphore.hpp
+++ ex3/Code_Skeleton/Part1/Semaphore.hpp
@@ -1,72 +1,62 @@
 #ifndef __SEMAPHORE_H
 #define __SEMAPHORE_H
+
 #include "Headers.hpp"
 #include <pthread.h>
+
 // Synchronization Warm up
 class Semaphore {
 public:
-	Semaphore(): semCnt(0){
-    queue<pthread_t> new_waiting;
-    waiting=new_waiting;
-		pthread_mutexattr_t attr;			//lock init
-		pthread_mutexattr_init(&attr);
-		pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
-		pthread_mutex_t error_check_mutex;
-		pthread_mutex_t error_check_mutex2;
-		pthread_mutex_init(&error_check_mutex, &attr);
-		pthread_mutex_init(&error_check_mutex2, &attr);
-		cntLock = error_check_mutex;
-		mainLock = error_check_mutex2;
+    Semaphore() : semCnt(0) {
+        queue<pthread_t> new_waiting;
+        waiting = new_waiting;
+        pthread_mutexattr_t attr;            //lock init
+        pthread_mutexattr_init(&attr);
+        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
+        pthread_mutex_t error_check_mutex;
+        pthread_mutex_init(&error_check_mutex, &attr);
+        cntLock = error_check_mutex;
+        pthread_cond_init(cond, NULL);
+    } // Constructs a new semaphore with a counter of 0
 
-  } // Constructs a new semaphore with a counter of 0
-
-	Semaphore(unsigned val): semCnt(val){
-    queue<pthread_t> new_waiting;
-    waiting=new_waiting;
-		pthread_mutexattr_t attr;		//lock init
-		pthread_mutexattr_init(&attr);
-		pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
-		pthread_mutex_t error_check_mutex;
-		pthread_mutex_t error_check_mutex2;
-		pthread_mutex_init(&error_check_mutex, &attr);
-		pthread_mutex_init(&error_check_mutex2, &attr);
-		cntLock = error_check_mutex;
-		mainLock = error_check_mutex2;
-  }; // Constructs a new semaphore with a counter of val
+    Semaphore(unsigned val) : semCnt(val) {
+        queue<pthread_t> new_waiting;
+        waiting = new_waiting;
+        pthread_mutexattr_t attr;            //lock init
+        pthread_mutexattr_init(&attr);
+        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
+        pthread_mutex_t error_check_mutex;
+        pthread_mutex_init(&error_check_mutex, &attr);
+        cntLock = error_check_mutex;
+        pthread_cond_init(cond, NULL);
+    } // Constructs a new semaphore with a counter of val
 
 
-	void up(){
-		pthread_mutex_lock(&cntLock);
-	  if (waiting.empty == true){
-	    semCnt++;
-			pthread_mutex_unlock(&cntLock);
-	    return;
-	  }
-	  pthread_t curr = waiting.pop();//getting the thread from the queue;
-	------
-		pthread_mutex_unlock(&cntLock);
-	} // Mark: 1 Thread has left the critical section
-  // sem post
+    void up() {
+        pthread_mutex_lock(&cntLock);
+        semCnt++;
+        pthread_cond_broadcast(cond);
+        pthread_mutex_unlock(&cntLock);
+    } // Mark: 1 Thread has left the critical section
+    // sem post
 
-	void down(){
-			pthread_mutex_lock(&cntLock);
-	    if(semCnt>0){
-	      semCnt--;
-	    }
-	    else{
-	      waiting.push(pthread_self()); //adding the thread to our wait list
-	      wait();
-	    }
-			pthread_mutex_unlock(&cntLock);
-
-	} // Block untill counter >0, and mark - One thread has entered the critical section.
-	//like sem_wait
+    void down() {
+        pthread_mutex_lock(&cntLock);
+        waiting.push(getpid());
+        while (semCnt == 0 || getpid() != waiting.front()) {
+            pthread_cond_wait(cond, &cntLock);
+        }
+        waiting.pop();
+        semCnt--;
+        pthread_mutex_unlock(&cntLock);
+    } // Block untill counter >0, and mark - One thread has entered the critical section.
+    //like sem_wait
 
 private:
-	unsigned int semCnt;
-  queue<pthread_t> waiting;
-	pthread_mutex_t cntLock;
-	pthread_mutex_t mainLock;
+    unsigned int semCnt;
+    queue<pthread_t> waiting;
+    pthread_mutex_t cntLock;
+    pthread_cond_t *cond;
 };
 
 #endif
